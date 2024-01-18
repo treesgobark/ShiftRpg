a39c4b8d9cfe4e0700e3dfc57ad6378cf03bc66f
treesgobark <loganpetro97@yahoo.com>
2024-01-18 17:26:08 -0500

    add screens and a player and an enemy and bullets

---
+++ ShiftRpg/Entities/Player.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Graphics.Particle;
+using FlatRedBall.Math.Geometry;
+using Microsoft.Xna.Framework;
+using ShiftRpg.Factories;
+
+namespace ShiftRpg.Entities
+{
+    public partial class Player
+    {
+        /// <summary>
+        /// Initialization logic which is executed only one time for this Entity (unless the Entity is pooled).
+        /// This method is called when the Entity is added to managers. Entities which are instantiated but not
+        /// added to managers will not have this method called.
+        /// </summary>
+        private void CustomInitialize()
+        {
+
+
+        }
+
+        private void CustomActivity()
+        {
+            if (InputDevice.DefaultPrimaryActionInput.WasJustPressed)
+            {
+                var dir = MovementInput.Position3D().NormalizedOrZero();
+                if (dir != Vector3.Zero)
+                {
+                    var bullet = BulletFactory.CreateNew();
+                    bullet.Position = Position;
+                    bullet.Velocity = dir * 100 + Velocity;
+                }
+            }
+        }
+
+        private void CustomDestroy()
+        {
+
+
+        }
+
+        private static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+    }
+}

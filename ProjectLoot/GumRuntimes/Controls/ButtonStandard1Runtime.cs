4840d6c47e92e1e6df2590d90b2bd81c41756e0f
treesgobark <loganpetro97@yahoo.com>
2024-06-18 22:28:21 -0400

    more new art direction UI stuff

---
+++ ProjectLoot/GumRuntimes/Controls/ButtonStandard1Runtime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectLoot.GumRuntimes.Controls
+{
+    public partial class ButtonStandard1Runtime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}

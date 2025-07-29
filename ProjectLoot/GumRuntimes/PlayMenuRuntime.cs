275e19987d24bdfe1c3788971cb55c73f49ad9d6
treesgobark <loganpetro97@yahoo.com>
2025-07-29 10:45:55 -0400

    improve menu navigation

---
+++ ProjectLoot/GumRuntimes/PlayMenuRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectLoot.GumRuntimes
+{
+    public partial class PlayMenuRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
--- ProjectLoot/Screens/MainMenu.cs
+++ ProjectLoot/Screens/MainMenu.cs
@@ -4,61 +4,63 @@ using FlatRedBall.Input;
 using FlatRedBall.Screens;
 using ProjectLoot.GumRuntimes;
 
-namespace ProjectLoot.Screens
+namespace ProjectLoot.Screens;
+
+public partial class MainMenu
 {
-    public partial class MainMenu
+    private void CustomInitialize()
     {
-        void CustomInitialize()
+        Xbox360GamePad? controller = InputManager.Xbox360GamePads[0];
+        if (controller.IsConnected)
         {
-            var controller = InputManager.Xbox360GamePads[0];
-            if (controller.IsConnected)
-            {
-                GuiManager.GamePadsForUiControl.Add(controller);
-                Forms.MainMenuInstance.PlayButton.IsFocused = true;
-            }
-
-            GumScreen.CurrentScreenState               =  MainMenuGumRuntime.Screen.Main;
-            Forms.MainMenuInstance.PlayButton.Click    += (_, _) => ScreenManager.MoveToScreen("TestLevel");
-            Forms.MainMenuInstance.OptionsButton.Click += OnClickOptions;
-            Forms.OptionsInstance.BackButton.Click     += OnClickBack;
-            Forms.MainMenuInstance.QuitButton.Click    += (_, _) => FlatRedBallServices.Game.Exit();
+            GuiManager.GamePadsForUiControl.Add(controller);
+            Forms.MainMenuInstance.PlayButton.IsFocused = true;
         }
 
-        private void OnClickBack(object o, EventArgs eventArgs)
+        GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Main;
+        
+        Forms.MainMenuInstance.PlayButton.Click +=
+            (_, _) => GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Play;
+        
+        Forms.MainMenuInstance.OptionsButton.Click += OnClickOptions;
+        Forms.MainMenuInstance.QuitButton.Click    += (_, _) => FlatRedBallServices.Game.Exit();
+        Forms.OptionsInstance.BackButton.Click     += OnClickBack;
+        Forms.PlayMenuInstance.ArenaButton.Click   += (_, _) => ScreenManager.MoveToScreen(nameof(Arena));
+        
+        Forms.PlayMenuInstance.TrainingGroundsButton.Click +=
+            (_, _) => ScreenManager.MoveToScreen(nameof(ShootingRange));
+        
+        Forms.PlayMenuInstance.BackButton.Click +=
+            (_, _) => GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Main;
+    }
+
+    private void OnClickBack(object? o, EventArgs eventArgs)
+    {
+        GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Main;
+        if (InputManager.Xbox360GamePads[0].IsConnected)
         {
-            GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Main;
-            if (InputManager.Xbox360GamePads[0].IsConnected)
-            {
-                Forms.MainMenuInstance.PlayButton.IsFocused = true;
-            }
+            Forms.MainMenuInstance.PlayButton.IsFocused = true;
         }
+    }
 
-        private void OnClickOptions(object o, EventArgs eventArgs)
+    private void OnClickOptions(object? o, EventArgs eventArgs)
+    {
+        GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Options;
+        if (InputManager.Xbox360GamePads[0].IsConnected)
         {
-            GumScreen.CurrentScreenState = MainMenuGumRuntime.Screen.Options;
-            if (InputManager.Xbox360GamePads[0].IsConnected)
-            {
-                Forms.OptionsInstance.BackButton.IsFocused = true;
-            }
+            Forms.OptionsInstance.BackButton.IsFocused = true;
         }
+    }
 
-        void CustomActivity(bool firstTimeCalled)
-        {
+    private void CustomActivity(bool firstTimeCalled)
+    {
+    }
 
+    private void CustomDestroy()
+    {
+    }
 
-        }
-
-        void CustomDestroy()
-        {
-
-
-        }
-
-        static void CustomLoadStaticContent(string contentManagerName)
-        {
-
-
-        }
-
+    private static void CustomLoadStaticContent(string contentManagerName)
+    {
     }
 }
